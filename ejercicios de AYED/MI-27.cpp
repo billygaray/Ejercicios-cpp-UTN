aa4dc5e9fce24b7e7b85c00b96c52bd3c15ddd5c
billygarayfrba <billygarayfrba@gmail.com>
2020-09-10 14:37:06 -0300

    puse el ejercico 27 pero no esta resuelto

---
+++ ejercicios de AYED/MI-27.cpp
@@ -0,0 +1,42 @@
+//============================================================================
+// Name        : MI-27.cpp
+// Author      : Billy Garay
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+/* En un torneo de futbol participan K equipos . El torneo se juega con el sistema de todos contra todos 
+. Por cada partido disputado por un equipo se dispone de la siguiente informacion :
+a) Nro de equipo.
+b) Codigo del resultado ( 'P' = Perdido , 'E' = Empatados , 'G' = Ganados ). 
+
+Se arma un lote de datos con todos los resultados del torneo , agrupados por  Nro de equipo.
+Desarrollar el progama que imprima : 
+    Por cada equipo, su numero y le puntaje total que obtuvo (suma 3 si gana, y 1 si empata) 
+*/
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main (){
+    cout << "ingresar la cantidad de equipos " << endl;
+    cin >> k ;
+    int v[k];
+
+    for (int i = 0; i < k ; i++)
+    {
+        v(i) = 0;  // mepa que esta mal
+        
+        for (int  j = 0; j <= (k-1) ; j++)
+        {
+            cout << "ingresar el resultaod del partido ";
+            cin >>     // me quede aqui de como asignar un CHAR 
+        }
+        
+
+
+    }
+    
+
+
+}
